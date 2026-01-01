1b1c1618e5be1af2ddfb15a8f9e78f36c8ac7b61
Jan Baykara <jan@commonknowledge.coop>
2026-01-01 16:22:04 +0000

    Fix getAllCollections

--- src/utils/zotero.ts
+++ src/utils/zotero.ts
@@ -3,12 +3,22 @@ export function getSelectedCollection() {
   return pane?.getSelectedCollection() || null;
 }
 
-export function getAllCollections() {
+export function getAllCollections(recursive = true) {
   const libraries = Array.from(Zotero.Libraries.getAll());
   const collections: Zotero.Collection[] = [];
   for (const library of libraries) {
     const libraryCollections = Zotero.Collections.getByLibrary(library.id);
     collections.push(...libraryCollections);
+  }
+  if (recursive) {
+    for (const collection of collections) {
+      collection.getDescendents().forEach((childObject) => {
+        if (childObject.type === "collection") {
+          const collection = Zotero.Collections.get(childObject.id);
+          collections.push(collection);
+        }
+      });
+    }
   }
   return collections;
 }
